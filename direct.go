0d4a81a10677c5173f41788a8519ec0a8dd5652f
jiangbo9510 <jiangbo1234321@gmail.com>
2024-01-08 00:46:03 +0800

    v1

---
+++ direct.go
@@ -0,0 +1,7 @@
+package test_go_direct_lib
+
+import "github.com/jiangbo9510/test_base_lib"
+
+func SetCommon(v string) {
+	test_base_lib.Common = ""
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/jiangbo9510/test_go_direct_lib
+
+go 1.18
+
+require github.com/jiangbo9510/test_base_lib v0.0.1
