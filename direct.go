cea2513a67892e5dba35d53961c381d16d4ea86c
jiangbo9510 <jiangbo1234321@gmail.com>
2024-01-09 11:30:03 +0800

    Update direct.go

--- direct.go
+++ direct.go
@@ -3,5 +3,9 @@ package test_go_direct_lib
 import "github.com/jiangbo9510/test_base_lib"
 
 func SetCommon(v string) {
-	test_base_lib.Common = ""
+	test_base_lib.Common = v
+}
+
+func GetVersion() string {
+	return test_base_lib.GetString()
 }
